7af8cca1b3abda2b8e2ea887a00569f31e9538c1
kapil-pandit <kapilpandit0408@gmail.com>
2024-07-24 20:25:07 +0530

    [FEAT] : "APIs added"

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route for GET requests
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello, world!' });
+});
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'test api' });
+});
+
+app.get('/api/public', (req, res) => {
+    res.json({ message: 'public api' });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
